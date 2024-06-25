ef08e878a9af0efaf54cc86ac2e5df9956d2bc59
jeffwang0516 <jeff1jeffo@gmail.com>
2024-06-25 17:26:20 +0800

    fix error

--- pr_agent/algo/utils.py
+++ pr_agent/algo/utils.py
@@ -499,7 +499,8 @@ def try_fix_yaml(response_text: str, keys_fix_yaml: List[str] = []) -> dict:
         try:
             data = yaml.safe_load(response_text_lines_tmp)
             get_logger().info(f"Successfully parsed AI prediction after removing last {i} lines")
-            # return data
+            if data is not None:
+                break
         except:
             pass
     if not data:
